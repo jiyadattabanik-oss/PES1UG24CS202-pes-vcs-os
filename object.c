53c8a6751538a2231ae8154dd044a1a2aa3fa362
jiyadattabanik-oss <jiya.datta.banik@gmail.com>
2026-04-21 20:11:31 +0530

    Update object.c

--- object.c
+++ object.c
@@ -94,6 +94,7 @@ int object_exists(const ObjectID *id) {
 //
 // Returns 0 on success, -1 on error.
 int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out) {
+    printf("object_write called\n");
     char header[64];
     const char *type_str;
 
